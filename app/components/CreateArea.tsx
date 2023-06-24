d4772a3037ee66067650d51ddfc993cfb3ee1186
Abiodun <emmi34.et@gmail.com>
2023-06-24 14:18:32 +0100

    update

--- app/components/CreateArea.tsx
+++ app/components/CreateArea.tsx
@@ -1,29 +1,3 @@
-// import React from "react";
-
-// function CreateArea() {
-//   return (
-//     <div>
-//       <form className="relative w-[95%] md:w-3/4 lg:w-8/12 xl:w-1/2 mt-8 mx-auto mb-5 bg-white p-4 rounded-lg shadow-md">
-//         <input
-//           name="title"
-//           placeholder="Title"
-//           className="w-full border-none p-1 outline-none text-xl resize-none"
-//         />
-//         <textarea
-//           name="content"
-//           placeholder="Take a note..."
-//           rows={3}
-//           className="w-full border-none p-1 outline-none text-xl resize-none"
-//         />
-//         <button 
-//          className="absolute right-5 bottom-[-18px] bg-[#f5ba13] border-none rounded-[50%] w-9 h-9 shadow-lg cursor-pointer
-//          outline-none text-white text-3xl">+</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default CreateArea;
 "use client"
 import React, { useState } from "react";
 
@@ -66,3 +40,30 @@ function CreateArea() {
 }
 
 export default CreateArea;
+
+// import React from "react";
+
+// function CreateArea() {
+//   return (
+//     <div>
+//       <form className="relative w-[95%] md:w-3/4 lg:w-8/12 xl:w-1/2 mt-8 mx-auto mb-5 bg-white p-4 rounded-lg shadow-md">
+//         <input
+//           name="title"
+//           placeholder="Title"
+//           className="w-full border-none p-1 outline-none text-xl resize-none"
+//         />
+//         <textarea
+//           name="content"
+//           placeholder="Take a note..."
+//           rows={3}
+//           className="w-full border-none p-1 outline-none text-xl resize-none"
+//         />
+//         <button 
+//          className="absolute right-5 bottom-[-18px] bg-[#f5ba13] border-none rounded-[50%] w-9 h-9 shadow-lg cursor-pointer
+//          outline-none text-white text-3xl">+</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default CreateArea;
